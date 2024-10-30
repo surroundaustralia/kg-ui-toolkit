0535a5f176187fc04ba3939c5d87c50dd11fd175
Rob Atkinson <ratkinson@ogc.org>
2024-10-31 09:30:17 +1100

    Bblocks metadata to incorporate in wider dependency tracing framework.  Does not impact app design.

---
+++ build.sh
@@ -0,0 +1,2 @@
+# Process building blocks
+docker run --pull=always --rm --workdir /workspace/kg-ui-toolkit -v "/mnt/c/repos/surround-gh:/workspace" ghcr.io/opengeospatial/bblocks-postprocess  --clean true
